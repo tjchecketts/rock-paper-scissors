df3d2d554f8a6e0a535caa4eade64e9e6f94e7ea
TJ Checketts <tylerchecketts@gmail.com>
2017-08-29 10:51:02 -0600

    fix jquery image change

--- rps.js
+++ rps.js
@@ -4,6 +4,10 @@ $(document).ready(function() {
     var rockChange;
     var paperChange;
     var scissorsChange;
+    var rockImg;
+    var paperImg;
+    var scissorsImg;
+    var imgSize = 'height="300"' + ' ' + 'width="300">'
 
     switch(this.value) {
       case 'rps':
@@ -22,52 +26,36 @@ $(document).ready(function() {
         scissorsChange = "Knight";
         break;
 
-    }
+    };
 
     $('#rock').text(rockChange);
     $('#paper').text(paperChange);
     $('#scissors').text(scissorsChange);
 
+    switch(this.value) {
+      case 'rps':
+        rockImg = '<img' + ' ' + 'src="rock.jpg"' + ' ' + 'alt="Rock cartoon hand sign"' + ' ' + imgSize;
+        paperImg = '<img' + ' ' + 'src="paper.jpg"' + ' ' + 'alt="Paper cartoon hand sign"' + ' ' + imgSize;
+        scissorsImg = '<img' + ' ' + 'src="scissors.jpg"' + ' ' + 'alt="Scissors cartoon hand sign"' + ' ' + imgSize;
+        break;
+      case 'bhn':
+        rockImg = '<img' + ' ' + 'src="bear.jpg"' + ' ' + 'alt="cartoon teddy bear"' + ' ' + imgSize;
+        paperImg = '<img' + ' ' + 'src="hunter.jpg"' + ' ' + 'alt="cartoon hunter Elmer Fudd"' + ' ' + imgSize;
+        scissorsImg = '<img' + ' ' + 'src="ninja.gif"' + ' ' + 'alt="cartoon ninja"' + ' ' + imgSize;
+        break;
+      case 'dpk':
+        rockImg = '<img' + ' ' + 'src="dragon.jpg"' + ' ' + 'alt="cartoon dragon breathing fire"' + ' ' + imgSize;
+        paperImg = '<img' + ' ' + 'src="princess.jpg"' + ' ' + 'alt="cartoon princess Anna"' + ' ' + imgSize;
+        scissorsImg = '<img' + ' ' + 'src="knight.jpg"' + ' ' + 'alt="cartoon knight on horse"' + ' ' + imgSize;
+        break;
+
+    };
+
+    $('.images').html(rockImg + paperImg + scissorsImg);
+
   })
-////////starting going down a bit of a rabbit hole...
-////////just leaving this her for now, maybe come back to it
-////////purpose was to switch out images for appropriate selector
-////////similar to text above
-
-  // $('#game').on('change', function() {
-  //   var rockImg;
-  //   var paperImg;
-  //   var scissorsImg;
-  //   var imgSize = 'height="250"' + ' ' + 'width="250">"'
-
-  //   switch(this.value) {
-  //     case 'rps':
-  //       rockImg = '<img' + ' ' + 'src="rock.jpg"' + ' ' + 'alt="Rock cartoon hand sign"' + ' ' + imgSize;
-  //       paperImg = '<img' + ' ' + 'paper.jpg"' + ' ' + 'alt="Paper cartoon hand sign"' + ' ' + imgSize;
-  //       scissorsImg = '<img' + ' ' + 'src="scissors.jpg"' + ' ' + 'alt="Scissors cartoon hand sign"' + ' ' + imgSize;
-  //       break;
-  //     case 'bhn':
-  //       rockImg = "Bear";
-  //       paperImg = "Hunter";
-  //       scissorsImg = '<img' + ' ' + 'src="ninja.jpg"' + ' ' + 'alt="cartoon ninja"' + ' ' + imgSize;
-  //       break;
-  //     case 'dpk':
-  //       rockImg = "Dragon";
-  //       paperImg = "Princess";
-  //       scissorsImg = "Knight";
-  //       break;
-
-  //   };
-
-  //   $('#rock_img').text('<img' + ' ' + 'src="ninja.jpg"' + ' ' + 'alt="cartoon ninja"' + ' ' + imgSize);
-  //   $('#paper_img').text('<img' + ' ' + 'src="ninja.jpg"' + ' ' + 'alt="cartoon ninja"' + ' ' + imgSize);
-  //   $('#scissors_img').text('<img' + ' ' + 'src="ninja.jpg"' + ' ' + 'alt="cartoon ninja"' + ' ' + imgSize);
-
-  // });
 
 })
-
-
 
 ///MECAHNICES OF RPS
 
