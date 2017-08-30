3cafef4566e6508b4dbdb3257c36ee2882479eda
TJ Checketts <tylerchecketts@gmail.com>
2017-08-29 19:02:03 -0600

    fix number randomizer, add js image click

--- rps.js
+++ rps.js
@@ -34,19 +34,28 @@ $(document).ready(function() {
 
     switch(this.value) {
       case 'rps':
-        rockImg = '<img' + ' ' + 'src="rock.jpg"' + ' ' + 'alt="Rock cartoon hand sign"' + ' ' + imgSize;
-        paperImg = '<img' + ' ' + 'src="paper.jpg"' + ' ' + 'alt="Paper cartoon hand sign"' + ' ' + imgSize;
-        scissorsImg = '<img' + ' ' + 'src="scissors.jpg"' + ' ' + 'alt="Scissors cartoon hand sign"' + ' ' + imgSize;
+        rockImg = '<img' + ' ' + 'id="rock_img"' + ' ' + 'src="rock.jpg"' + ' ' 
+          + 'alt="Rock cartoon hand sign"' + ' ' + imgSize;
+        paperImg = '<img' + ' ' + 'id="paper_img"' + ' ' + 'src="paper.jpg"' + ' ' 
+          + 'alt="Paper cartoon hand sign"' + ' ' + imgSize;
+        scissorsImg = '<img' + ' ' + 'id="scissors_img"' + ' ' + 'src="scissors.jpg"' + ' ' 
+          + 'alt="Scissors cartoon hand sign"' + ' ' + imgSize;
         break;
       case 'bhn':
-        rockImg = '<img' + ' ' + 'src="bear.jpg"' + ' ' + 'alt="cartoon teddy bear"' + ' ' + imgSize;
-        paperImg = '<img' + ' ' + 'src="hunter.jpg"' + ' ' + 'alt="cartoon hunter Elmer Fudd"' + ' ' + imgSize;
-        scissorsImg = '<img' + ' ' + 'src="ninja.gif"' + ' ' + 'alt="cartoon ninja"' + ' ' + imgSize;
+        rockImg = '<img' + ' ' + 'id="rock_img"' + ' ' + 'src="bear.jpg"' + ' ' 
+          + 'alt="cartoon teddy bear"' + ' ' + imgSize;
+        paperImg = '<img' + ' ' + 'id="paper_img"' + ' ' + 'src="hunter.jpg"' + ' ' 
+          + 'alt="cartoon hunter Elmer Fudd"' + ' ' + imgSize;
+        scissorsImg = '<img' + ' ' + 'id="scissors_img"' + ' ' + 'src="ninja.gif"' + ' ' 
+          + 'alt="cartoon ninja"' + ' ' + imgSize;
         break;
       case 'dpk':
-        rockImg = '<img' + ' ' + 'src="dragon.jpg"' + ' ' + 'alt="cartoon dragon breathing fire"' + ' ' + imgSize;
-        paperImg = '<img' + ' ' + 'src="princess.jpg"' + ' ' + 'alt="cartoon princess Anna"' + ' ' + imgSize;
-        scissorsImg = '<img' + ' ' + 'src="knight.jpg"' + ' ' + 'alt="cartoon knight on horse"' + ' ' + imgSize;
+        rockImg = '<img' + ' ' + 'id="rock_img"' + ' ' + 'src="dragon.jpg"' + ' ' 
+          + 'alt="cartoon dragon breathing fire"' + ' ' + imgSize;
+        paperImg = '<img' + ' ' + 'id="paper_img"' + ' ' + 'src="princess.jpg"' + ' ' 
+          + 'alt="cartoon princess Anna"' + ' ' + imgSize;
+        scissorsImg = '<img' + ' ' + 'id="scissors_img"' + ' ' + 'src="knight.jpg"' + ' ' 
+          + 'alt="cartoon knight on horse"' + ' ' + imgSize;
         break;
 
     };
@@ -55,23 +64,33 @@ $(document).ready(function() {
 
   })
 
-})
+  ///MECAHNICES OF RPS
 
-///MECAHNICES OF RPS
+  $('#rock_img').on('click', function() {
+    var rndm_num = Math.floor(Math.random() * 3 + 1)
+    var yourChoice = 1;
+    
+    console.log(yourChoice);
+    console.log(rndm_num)
 
-var rndm_num = Math.floor(Math.random() * 3 + 1)
-var oppenent = [];
-switch(rndm_num) {
-  case rndm_num === 1:
-    opponent = 1
-    break;
-  case rndm_num === 2:
-    opponent = 2
-    break;
-  case rndm_num === 3:
-    opponent = 3
-    break;
+  });
 
-};
+  $('#paper_img').on('click', function() {
+    var rndm_num = Math.floor(Math.random() * 3 + 1)
+    var yourChoice = 2;
+    
+    console.log(yourChoice);
+    console.log(rndm_num)
 
-// console.log(opponent)
+  });
+
+  $('#scissors_img').on('click', function() {
+    var rndm_num = Math.floor(Math.random() * 3 + 1)
+    var yourChoice = 3;
+    
+    console.log(yourChoice);
+    console.log(rndm_num)
+
+  }) ; 
+
+});
